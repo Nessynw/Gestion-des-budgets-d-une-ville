5c838a95112695aa6c29d14c55cb02cba46f1bb8
Elena <elri1076@gmail.com>
2025-11-25 13:47:59 +0100

    Version complétée du package VersSacADos

--- src/main/java/sacADos/VersSacADos.java
+++ src/main/java/sacADos/VersSacADos.java
@@ -1,85 +1,94 @@
 package sacADos;
-import java.util.List;
-import java.util.Arrays;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.*;
+import  java.lang.*;
 import equipe.Projet;
 
+
 public class VersSacADos{
-    //A COMPLETER avec les méthodes des packages gloutons pour convertir en sacados
 
     public SacADos conversionParCouts(Budget budget,List<Projet> projets) {
 //Les budgets concernent les 3 différents types de coûts (économique, social, et environnemental)
         int[] tabBudgets=budget.getBudgetCouts();
-        SacADos sac=new SacADos(tabBudgets,projets);
+        SacADos sac = new SacADos(tabBudgets,projets);
         return sac;
     }
 
-    public SacADos conversionParSecteur(Budget budget,(List<Projet> projets) {
+    public SacADos conversionParSecteur(Budget budget,List<Projet> projets) {
 //Les budgets concernent les 5 dfférents secteurs et seul le coût économique est considéré
         int[] tabBudgets=budget.getBudgetSecteurs();
         SacADos sac=new SacADos(tabBudgets,projets);
         return sac;
     }
 
-    public SacADos convertir(String namefile){//pas encore fini
+    public SacADos convertir(String namefile){
         try {
             FileInputStream f = new FileInputStream(new File(namefile));
-            BufferedReader reader=new BufferedReader(new FileReader(new File(namefile))) ;
+            BufferedReader reader = new BufferedReader(new FileReader(new File(namefile)));
 
-            String line=reader.readLine(); //premiere ligne du fichier
-            String[] valeurs=line.split(" ");
-            int nbObjets=Integer.parseInt(valeurs[0]);
-            int nbBudgets=Integer.parseInt(valeurs[1]);
-            if(valeurs.length()==3){
-                int valOptimale=Integer.parseInt(valeurs[2]);
-            }
-            else{
-                int valOptimale=0;
+            String line = reader.readLine(); //premiere ligne du fichier
+            String[] valeurs = line.split(" ");
+            int nbObjets = Integer.parseInt(valeurs[0]);
+            int nbBudgets = Integer.parseInt(valeurs[1]);
+            if (valeurs.length == 3) {
+                int valOptimale = Integer.parseInt(valeurs[2]);
+            } else {
+                int valOptimale = 0;
             }
 
             List<Objet> objets = new ArrayList<>();
 
             //lignes des utilites de chaque objet
-            int i=0;
-            while(i<nbObjets){
-                String line=reader.readLine();
-                String[] utilites=line.split(" ");
-                for (int j=0;j<utilites.length();j++) {
-                    int val=Integer.parseInt(utilites[j]);
-                    Objet o=Objet("",val,[]);
+            int i = 0;
+            while (i < nbObjets) {
+                line = reader.readLine();
+                String[] utilites = line.split(" ");
+                for (int j = 0; j < utilites.length; j++) {
+                    int val = Integer.parseInt(utilites[j]);
+                    Objet o = new Objet("", val);
                     objets.add(o);
                 }
-                i+=utilites.length();
+                i += utilites.length;
             }
 
             //lire les couts pour chaque objet
-            int i=0;
-            while (i<nbObjets){
-                int l=0;
-                do{
-                    String line=reader.readLine();
-                    String[] couts=line.split(" ");
-                    l+=couts.length();
-                }
-                while(couts.length()<nbBudgets);
+            i = 0;
+            int l = 0;//compteur nombre de couts selon le flux
+            //int[] couts=new int[nbBudgets];
+            List<String> coutslus = new ArrayList<>();
+            while (i < nbObjets) {
 
-                //A COMPLETER
+                do {
+                    line = reader.readLine();
+                    String[] couts2 = line.split(" ");
+                    Collections.addAll(coutslus, couts2);
+                    l += coutslus.size();
+                }
+                while (l < nbBudgets);
+
+                Objet o = objets.get(i);
+                for (int j = 0; j < nbBudgets; j++) {
+                    int val = Integer.parseInt(coutslus.get(j));
+                    o.addCout(val);
+                    coutslus.removeFirst();//on retire un cout a chaque fois qu'on le rajoute a l'objet
+                }
+
+                l -= nbBudgets;
                 i++;
             }
 
             //lire les budgets
-            String line=reader.readLine();
-            String[] valeurs=line.split(" ");
-            int[] budgets=new int[valeurs.length()];
-            for (int i=0;i<valeurs.length();i++){
-                budgets[i]=Integer.parseInt(valeurs[j]);
+            line = reader.readLine();
+            valeurs = line.split(" ");
+            int[] budgets = new int[valeurs.length];
+            for (i = 0; i < valeurs.length; i++) {
+                budgets[i] = Integer.parseInt(valeurs[i]);
             }
 
             reader.close();
-
-            SacADos sac=new SacADos(budgets,objets);
+            f.close();
+            SacADos sac = new SacADos(budgets, objets);
+            return sac;
         }
         catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -87,11 +96,13 @@ public class VersSacADos{
         catch(IOException e){
             e.printStackTrace();
         }
-
-        f.close();
-        return sac;
+        SacADos sacvide=new SacADos();
+        return sacvide;
     }
 
 
+
 }
+
+
 
