e498bd8c8f4df93be04dc5c0aafcee444075dd9c
LydiaNed <lydianedirlydia@gmail.com>
2025-11-27 12:57:58 +0100

    Dans Comparateur, j'ai définis comment comparer deux objets : je regarde leur utilité et je trie les plus utiles en premier. Ça permet à mon GloutonAjoutSolver de savoir quel objet prendre en priorité pour remplir le sac sans dépasser le budget.

--- src/main/java/solveur/glouton/Comparateur.java
+++ src/main/java/solveur/glouton/Comparateur.java
@@ -1,4 +1,13 @@
 package solveur.glouton;
 
+import sacADos.Objet;
+import java.util.Comparator;
+
+
 public class Comparateur {
+    public int compare(Objet o1, Objet o2) {
+        // ici jai trié par utilite decroissante ( en gros le plus utile)
+        return o2.getUtilite() - o1.getUtilite();
+    }
 }
+
